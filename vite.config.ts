9932695974c063558b0e97f41aa8f9cfb616b67c
Afsoon <aslani.a7260@gmail.com>
2025-10-06 14:39:32 +0330

    fix ci

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-	base: '/afsoonaslanii.github.io/',
+	base: '/',
 	server: {
 		host: '::',
 		port: 8081,
